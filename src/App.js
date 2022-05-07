f8667f50a2794eec8c13f841cf0eeebdee035b9c
Bharath Gowda <bharath1234gowda@gmail.com>
2022-05-07 11:12:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MainArea from './components/MainArea';
+import SideNav from './components/SideNav';
 function App() {
+  const something=<div>somehthing</div>
+  console.log(something)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <SideNav/>
+      <MainArea x="10"/>
     </div>
   );
 }
---
+++ src/components/MainArea.js
@@ -0,0 +1,11 @@
+
+import React from "react"
+import "../App.css"
+export default function MainArea(props){
+    // console.log(x)
+    return (
+        <div>
+            something
+        </div>
+    )
+}
